503a58ac15b945d7db06399498e7bf0d1f92b891
Namatuzio <lemonadewalkerz@gmail.com>
2021-09-13 02:46:20 -0400

    Added level reload
    Reloads the level on a fail condition (touching the ground)

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
@@ -18,8 +19,14 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Home at last!");
                 break;
             default:
-                Debug.Log("I blew up!");
+                ReloadLevel();
                 break;
         }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
